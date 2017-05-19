b19a5ba145289bdc63c7352020b646bb8ee7199c
Krunk <cable2999@users.noreply.github.com>
2017-05-19 14:01:35 -0500

    Raising level now sets learned skills to 70%

--- code/update.c
+++ code/update.c
@@ -121,6 +121,13 @@ void advance_level( CHAR_DATA *ch, bool hide )
 		}
 	}
 
+        for (sn = 0; sn < MAX_SKILL; sn++) {
+		if (ch->pcdata->learned[sn] == 1 && skill_table[sn].ctype != CMD_POWER){
+                        ch->pcdata->learned[sn] = 70;
+                }
+        }
+
+
 	for (i=0;i<100;i++) {
 		if (ch->pcdata->recentkills[i] && ch->pcdata->recentkills[i][0] != '\0' && strcmp(ch->pcdata->recentkills[i],""))
 		{
