fb28f7346e69d149b2804a8d03f2c34f4f511ede
cable2999 <cable2999@users.noreply.github.com>
2017-06-07 13:37:04 -0500

    Tabs to spaces

--- code/affect.h
+++ code/affect.h
@@ -1,48 +1,48 @@
 class CAffect
 {
 public:
-	CAffect();
-	~CAffect();
+    CAffect();
+    ~CAffect();
 
-	CCharacter *		GetOwner();
-	RString &			GetName();
-	
-	CAffect *			next;			//next on target of affect, i.e. next affect on char
-	
-	void				SetName(RString &nName);
-	void				SetOwner(CCharacter *nOwner);
-	
-	bool				valid;
+    CCharacter *        GetOwner();
+    RString &           GetName();
+    
+    CAffect *           next;           //next on target of affect, i.e. next affect on char
+    
+    void                SetName(RString &nName);
+    void                SetOwner(CCharacter *nOwner);
+    
+    bool                valid;
 
-	int					type;
-	
-	int					where;
-	int					level;
-	int					duration;
-	int					location;
-	int					modifier;
-	int					aftype;
+    int                 type;
+    
+    int                 where;
+    int                 level;
+    int                 duration;
+    int                 location;
+    int                 modifier;
+    int                 aftype;
 
-	CBitvector			bitvector;
-	CBitvector			properties;		//permanent?  a malady?  etc
-	
-	CFuncPtr			pulse_fun;
-	CFuncPtr			tick_fun;
-	CFuncPtr			end_fun;		//triggers on affect removal.  DO NOT DEPEND ON (due to crashes).
+    CBitvector          bitvector;
+    CBitvector          properties;     //permanent?  a malady?  etc
+    
+    CFuncPtr            pulse_fun;
+    CFuncPtr            tick_fun;
+    CFuncPtr            end_fun;        //triggers on affect removal.  DO NOT DEPEND ON (due to crashes).
 
-	void				Apply(CCharacter *ch); //overloading rocks your fucking socks off
-	void				Apply(CObject *ch);
-	void				Apply(CRoom *ch);
-	void				Apply(CArea *ch);
+    void                Apply(CCharacter *ch); //overloading rocks your fucking socks off
+    void                Apply(CObject *ch);
+    void                Apply(CRoom *ch);
+    void                Apply(CArea *ch);
 
-	enum
-	{
-		TARG_CHAR, TARG_OBJ, TARG_ROOM, TARG_AREA	//affect target types
-	};
-	void				RemoveAffect(); //eliminates results of affect, i.e. -str and calls FreeAffect()
+    enum
+    {
+        TARG_CHAR, TARG_OBJ, TARG_ROOM, TARG_AREA   //affect target types
+    };
+    void                RemoveAffect(); //eliminates results of affect, i.e. -str and calls FreeAffect()
 protected:
-	void *				target;			//who the affect is placed on
-	int					target_type;
-	CCharacter *		owner;
-	RString				name;
+    void *              target;         //who the affect is placed on
+    int                 target_type;
+    CCharacter *        owner;
+    RString             name;
 };
