27a0a1450d7442f812bc5be0188afe6052fa1dc6
Joel Franzen <jfranzen@illinois.edu>
2017-06-07 13:47:12 -0500

    Tabs to spaces

--- code/bit.c
+++ code/bit.c
@@ -41,56 +41,56 @@ struct flag_stat_type
 
 
 /*****************************************************************************
- Name:		flag_stat_table
- Purpose:	This table catagorizes the tables following the lookup
- 		functions below into stats and flags.  Flags can be toggled
- 		but stats can only be assigned.  Update this table when a
- 		new set of flags is installed.
+ Name:      flag_stat_table
+ Purpose:   This table catagorizes the tables following the lookup
+        functions below into stats and flags.  Flags can be toggled
+        but stats can only be assigned.  Update this table when a
+        new set of flags is installed.
  ****************************************************************************/
 const struct flag_stat_type flag_stat_table[] =
 {
-/*  {	structure		stat	}, */
-    {	area_flags,		FALSE	},
-    {   sex_flags,		TRUE	},
-    {   exit_flags,		FALSE	},
-    {   door_resets,		TRUE	},
-    {   room_flags,		FALSE	},
-    {   sector_flags,		TRUE	},
-    {   type_flags,		TRUE	},
-    {   extra_flags,		FALSE	},
-    {   wear_flags,		FALSE	},
-    {   act_flags,		FALSE	},
-    {   affect_flags,		FALSE	},
-    {   apply_flags,		TRUE	},
-    {   wear_loc_flags,		TRUE	},
-    {   wear_loc_strings,	TRUE	},
-    {   weapon_flags,		TRUE	},
-    {   container_flags,	FALSE	},
+/*  {   structure           stat    }, */
+    {   area_flags,         FALSE   },
+    {   sex_flags,          TRUE    },
+    {   exit_flags,         FALSE   },
+    {   door_resets,        TRUE    },
+    {   room_flags,         FALSE   },
+    {   sector_flags,       TRUE    },
+    {   type_flags,         TRUE    },
+    {   extra_flags,        FALSE   },
+    {   wear_flags,         FALSE   },
+    {   act_flags,          FALSE   },
+    {   affect_flags,       FALSE   },
+    {   apply_flags,        TRUE    },
+    {   wear_loc_flags,     TRUE    },
+    {   wear_loc_strings,   TRUE    },
+    {   weapon_flags,       TRUE    },
+    {   container_flags,    FALSE   },
 
 /* ROM specific flags: */
 
-    {   material_type,          TRUE    },
-    {   form_flags,             FALSE   },
-    {   part_flags,             FALSE   },
-    {   ac_type,                TRUE    },
-    {   size_flags,             TRUE    },
-    {   position_flags,         TRUE    },
-    {   off_flags,              FALSE   },
-    {   imm_flags,              FALSE   },
-    {   res_flags,              FALSE   },
-    {   vuln_flags,             FALSE   },
-    {   weapon_class,           TRUE    },
-    {   weapon_type2,            FALSE   },
-    {   0,			0	}
+    {   material_type,      TRUE    },
+    {   form_flags,         FALSE   },
+    {   part_flags,         FALSE   },
+    {   ac_type,            TRUE    },
+    {   size_flags,         TRUE    },
+    {   position_flags,     TRUE    },
+    {   off_flags,          FALSE   },
+    {   imm_flags,          FALSE   },
+    {   res_flags,          FALSE   },
+    {   vuln_flags,         FALSE   },
+    {   weapon_class,       TRUE    },
+    {   weapon_type2,       FALSE   },
+    {   0,          0   }
 };
 
 
 
 /*****************************************************************************
- Name:		is_stat( table )
- Purpose:	Returns TRUE if the table is a stat table and FALSE if flag.
- Called by:	flag_value and flag_string.
- Note:		This function is local and used only in bit.c.
+ Name:      is_stat( table )
+ Purpose:   Returns TRUE if the table is a stat table and FALSE if flag.
+ Called by: flag_value and flag_string.
+ Note:      This function is local and used only in bit.c.
  ****************************************************************************/
 bool is_stat( const struct flag_type *flag_table )
 {
@@ -98,9 +98,9 @@ bool is_stat( const struct flag_type *flag_table )
 
     for (flag = 0; flag_stat_table[flag].structure; flag++)
     {
-	if ( flag_stat_table[flag].structure == flag_table
-	  && flag_stat_table[flag].stat )
-	    return TRUE;
+    if ( flag_stat_table[flag].structure == flag_table
+      && flag_stat_table[flag].stat )
+        return TRUE;
     }
     return FALSE;
 }
@@ -113,10 +113,10 @@ bool is_stat( const struct flag_type *flag_table )
  * has NOT been granted.  Use only in this OLC package has been granted.
  */
 /*****************************************************************************
- Name:		flag_lookup2( flag, table )
- Purpose:	Returns the value of a single, settable flag from the table.
- Called by:	flag_value and flag_string.
- Note:		This function is local and used only in bit.c.
+ Name:      flag_lookup2( flag, table )
+ Purpose:   Returns the value of a single, settable flag from the table.
+ Called by: flag_value and flag_string.
+ Note:      This function is local and used only in bit.c.
  ****************************************************************************/
 int flag_lookup2 (const char *name, const struct flag_type *flag_table)
 {
@@ -133,29 +133,29 @@ int flag_lookup2 (const char *name, const struct flag_type *flag_table)
 }
 
 /*****************************************************************************
- Name:		flag_value( table, flag )
- Purpose:	Returns the value of the flags entered.  Multi-flags accepted.
- Called by:	olc.c and olc_act.c.
+ Name:      flag_value( table, flag )
+ Purpose:   Returns the value of the flags entered.  Multi-flags accepted.
+ Called by: olc.c and olc_act.c.
  ****************************************************************************/
 int flag_value( const struct flag_type *flag_table, char *argument)
 {
     char word[MAX_INPUT_LENGTH];
     int  bit;
 
-	one_argument( argument, word );
+    one_argument( argument, word );
 
-	if ( ( bit = flag_lookup2( word, flag_table ) ) != NO_FLAG )
-	    return bit;
-	else
-	    return NO_FLAG;
+    if ( ( bit = flag_lookup2( word, flag_table ) ) != NO_FLAG )
+        return bit;
+    else
+        return NO_FLAG;
 }
 
 
 
 /*****************************************************************************
- Name:		flag_string( table, flags/stat )
- Purpose:	Returns string with name(s) of the flags or stat entered.
- Called by:	act_olc.c, olc.c, and olc_save.c.
+ Name:      flag_string( table, flags/stat )
+ Purpose:   Returns string with name(s) of the flags or stat entered.
+ Called by: act_olc.c, olc.c, and olc_save.c.
  ****************************************************************************/
 char *flag_string( const struct flag_type *flag_table, long bits[] )
 {
@@ -166,60 +166,60 @@ char *flag_string( const struct flag_type *flag_table, long bits[] )
 
     for (flag = 0; flag_table[flag].name != NULL; flag++)
     {
-		if ( !is_stat( flag_table ) && IS_SET(bits, flag_table[flag].bit) )
-		{
-	    	strcat( buf, " " );
-	    	strcat( buf, flag_table[flag].name );
-		}
-		else if ( IS_SET(bits, flag_table[flag].bit))
-		{
-	    	strcat( buf, " " );
-	    	strcat( buf, flag_table[flag].name );
-	    	break;
-		}
+        if ( !is_stat( flag_table ) && IS_SET(bits, flag_table[flag].bit) )
+        {
+            strcat( buf, " " );
+            strcat( buf, flag_table[flag].name );
+        }
+        else if ( IS_SET(bits, flag_table[flag].bit))
+        {
+            strcat( buf, " " );
+            strcat( buf, flag_table[flag].name );
+            break;
+        }
     }
     return (buf[0] != '\0') ? buf+1 : (char*)"none";
 }
 
 char *style_string( const struct style_type *style_table, long bits[] )
 {
-	static char buf[512];
-	int flag;
+    static char buf[512];
+    int flag;
 
-	buf[0] = '\0';
+    buf[0] = '\0';
 
-	for(flag = 0; flag < MAX_STYLE; flag++)
-	{
-		if( IS_SET(bits, style_table[flag].bit))
-		{
-			if(flag > 0)
-				strcat( buf, " ");
-			strcat( buf, style_table[flag].name);
-		}
-	}
+    for(flag = 0; flag < MAX_STYLE; flag++)
+    {
+        if( IS_SET(bits, style_table[flag].bit))
+        {
+            if(flag > 0)
+                strcat( buf, " ");
+            strcat( buf, style_table[flag].name);
+        }
+    }
 
-	return (buf[0] != '\0') ? buf+1 : (char*)"none";
+    return (buf[0] != '\0') ? buf+1 : (char*)"none";
 
 }
 
 char *restrict_string( const struct restrict_type *restrict_table, long bits[] )
 {
-	static char buf[512];
-	int flag;
+    static char buf[512];
+    int flag;
 
-	buf[0] = '\0';
+    buf[0] = '\0';
 
-	for(flag = 0; restrict_table[flag].name != NULL; flag++)
-	{
-		if( IS_SET(bits, restrict_table[flag].bit))
-		{
-			if(flag > 0)
-				strcat( buf, " ");
-			strcat( buf, restrict_table[flag].name);
-		}
-	}
+    for(flag = 0; restrict_table[flag].name != NULL; flag++)
+    {
+        if( IS_SET(bits, restrict_table[flag].bit))
+        {
+            if(flag > 0)
+                strcat( buf, " ");
+            strcat( buf, restrict_table[flag].name);
+        }
+    }
 
-	return (buf[0] != '\0') ? buf+1 : (char*)"none";
+    return (buf[0] != '\0') ? buf+1 : (char*)"none";
 
 }
 
@@ -232,18 +232,18 @@ char *flag_string_old( const struct flag_type *flag_table, int bits)
 
     for (flag = 0; flag_table[flag].name != NULL; flag++)
     {
-		if ( !is_stat( flag_table ) && IS_SET_OLD(bits, flag_table[flag].bit) )
-		{
-	    	strcat( buf, " " );
-	    	strcat( buf, flag_table[flag].name );
-		}
-		else
-			if ( flag_table[flag].bit == bits )
-		{
-	    	strcat( buf, " " );
-	    	strcat( buf, flag_table[flag].name );
-	    	break;
-		}
+        if ( !is_stat( flag_table ) && IS_SET_OLD(bits, flag_table[flag].bit) )
+        {
+            strcat( buf, " " );
+            strcat( buf, flag_table[flag].name );
+        }
+        else
+            if ( flag_table[flag].bit == bits )
+        {
+            strcat( buf, " " );
+            strcat( buf, flag_table[flag].name );
+            break;
+        }
     }
     return (buf[0] != '\0') ? buf+1 : (char*)"none";
 }
--- code/class.h
+++ code/class.h
@@ -17,48 +17,48 @@ class CClass //no redundancies here.
 {
 friend class CMud;
 public:
-		CClass();
-		~CClass();
+        CClass();
+        ~CClass();
  
-		CClass *		next;
-		static CClass * first;
-		static CClass * GetClass(int nIndex);			//get class ptr based on index, i.e. you pass CLASS_PALADIN
-		int				GetIndex();	//get index of class, i.e. it returns CLASS_PALADIN
-		CClass *		operator[](int nIndex);			//same as Getclass
-		static int 		Lookup (const char *name);		//lookup "paladin" and return CLASS_PALADIN
+        CClass *        next;
+        static CClass * first;
+        static CClass * GetClass(int nIndex);           //get class ptr based on index, i.e. you pass CLASS_PALADIN
+        int             GetIndex(); //get index of class, i.e. it returns CLASS_PALADIN
+        CClass *        operator[](int nIndex);         //same as Getclass
+        static int      Lookup (const char *name);      //lookup "paladin" and return CLASS_PALADIN
 
-		RString			name;
-		RString			who_name;			//3 letter name that shows on who
+        RString         name;
+        RString         who_name;           //3 letter name that shows on who
 
-		static void		LoadClassTable(const char *query);
-		
-		int				attr_prime;				//prime attrib
-		int				align;
-		int				weapon;					//weapon you start with
-		int				gainconst;
-		
-		int				ctype;					//neither, caster, communer
-		int				status;					//open for players to choose?
+        static void     LoadClassTable(const char *query);
+        
+        int             attr_prime;             //prime attrib
+        int             align;
+        int             weapon;                 //weapon you start with
+        int             gainconst;
+        
+        int             ctype;                  //neither, caster, communer
+        int             status;                 //open for players to choose?
 
-		RString			base_group;
-		RString			default_group;
-/*		enum
-		{
-			CLASS_NEITHER = 0,
-			CLASS_CASTER,
-			CLASS_COMMUNER
-		};
+        RString         base_group;
+        RString         default_group;
+/*      enum
+        {
+            CLASS_NEITHER = 0,
+            CLASS_CASTER,
+            CLASS_COMMUNER
+        };
 
-		enum
-		{
-			ALIGN_ANY = 0,
-			ALIGN_G,
-			ALIGN_N,
-			ALIGN_E,
-			ALIGN_GN,
-			ALIGN_GE,
-			ALIGN_NE
-		};
-*/		
-		int				index;
+        enum
+        {
+            ALIGN_ANY = 0,
+            ALIGN_G,
+            ALIGN_N,
+            ALIGN_E,
+            ALIGN_GN,
+            ALIGN_GE,
+            ALIGN_NE
+        };
+*/      
+        int             index;
 };
