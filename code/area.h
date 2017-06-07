cb485583cf35de75f537d90b5de5eeda2978f7b9
Joel Franzen <jfranzen@illinois.edu>
2017-06-07 13:37:04 -0500

    Tabs to spaces

--- code/area.h
+++ code/area.h
@@ -1,42 +1,42 @@
 #include "rift.h"
 #include "weather.h"
 
-#define MAX_ADJACENT	25
+#define MAX_ADJACENT    25
 
 class CArea
 {
 public:
-	CArea();
-	~CArea();
-	CArea *			next;
-	static CArea *	first;
-	
-	static void		LoadAreaData();
-	static void		LoadAreaTable(const char *query); /*this is reserved for bootstrap use*/
-	
-	int				GetPlayerCount(void);
-	int				GetAreaAge(void);
-	CAreaAffect *	GetFirstAffect(void);
-	
-	short int		low_range;	/* lowest safe level */
-	short int		high_range; /* highest useful level */
-	short int		area_type;
-	short int		index;
+    CArea();
+    ~CArea();
+    CArea *         next;
+    static CArea *  first;
+    
+    static void     LoadAreaData();
+    static void     LoadAreaTable(const char *query); /*this is reserved for bootstrap use*/
+    
+    int             GetPlayerCount(void);
+    int             GetAreaAge(void);
+    CAreaAffect *   GetFirstAffect(void);
+    
+    short int       low_range;  /* lowest safe level */
+    short int       high_range; /* highest useful level */
+    short int       area_type;
+    short int       index;
 
-	short int		min_vnum;
-	short int		max_vnum;
-	
-	short int		type;
-	short int		climate;
-	
-	RString			name;
-	RString			area_file;
-	RString			credits;
+    short int       min_vnum;
+    short int       max_vnum;
+    
+    short int       type;
+    short int       climate;
+    
+    RString         name;
+    RString         area_file;
+    RString         credits;
 
-	CBitvector		flags;
+    CBitvector      flags;
 
-	CArea *			adjacent_areas[MAX_ADJACENT];
-	CWeather *		area_weather;
+    CArea *         adjacent_areas[MAX_ADJACENT];
+    CWeather *      area_weather;
 private:
-	int				area_age;
+    int             area_age;
 };
