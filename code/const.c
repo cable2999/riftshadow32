ac921cf948864543c53b59aef07c0f84a4e6e91c
cable2999 <cable2999@users.noreply.github.com>
2017-06-26 16:23:39 -0500

    Dropping parry levels for everybody.

--- code/const.c
+++ code/const.c
@@ -1698,7 +1698,7 @@ const   struct  skill_type  skill_table [MAX_SKILL] =
     },
 
     {
-        "parry", {60, 1, 13, 7, 1, 1, 1, 20, 18, 21, 18, 6},
+        "parry", {60, 1, 4, 7, 1, 1, 1, 7, 6, 7, 6, 6},
         spell_null,             TAR_IGNORE,             POS_FIGHTING,
         &gsn_parry,             0,        0,     0,
         "",                     "!Parry!",      "", CMD_NONE
